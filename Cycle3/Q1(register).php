a7fa92c707aeb562426b04eab0385f3256661ca2
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-13 20:54:52 +0530

    new

--- Cycle3/Q1(register).php
+++ Cycle3/Q1(register).php
@@ -1,149 +1,22 @@
-
-
 <html>
-
-<body bgcolor="" >
-<form name="myform" method="post" action="" enctype="multipart/form-data">
-  
-
-    <tr>
-      
-          <td width="74%" valign="top"><table width="739" height="895" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#000033" bgcolor="#FFFFFF">
-            <tr height="">
-              <td height="auto" colspan="6" align="center" bordercolor="#CC0000" bgcolor=""></td>
-            </tr>
-            <tr>
-              <td align="right" height="44">
-			  <div align="center" class="style5 style15"><strong>Name</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="y_name" type="text" size="50" placeholder="Enter Name......" />
-		          <br>
-                </div></td>
-            </tr>
-			
-            <tr>
-              <td height="39" align="right">
-			  <div align="center" class="style5 style15"><strong>Gender </strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <label>
-		          <select name="gender">
-		            <option>Male</option>
-		            <option>Female</option>
-	              </select>
-		          </label>
-		        </div></td>
-            </tr>
-            
-            <tr>
-              <td height="33" align="right">
-			  <div align="center" class="style5 style15"><strong>Date of Birth </strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="dob" type="date" size="50" placeholder="Enter date of birth ......" />
-                </div></td>
-            </tr>
-            <tr>
-              <td height="32" align="right">
-			  <div align="center" class="style5 style15"><strong>Father's Name</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="f_name" type="text" size="50" placeholder="Enter Father's Name......" />
-                </div></td>
-            </tr>
-            <tr>
-              <td height="33" align="right">
-			  <div align="center" class="style5 style15"><strong>Nationality</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="nationality" type="text" size="50" placeholder="Enter the Nationality......" />
-                </div></td>
-            </tr>
-		
-			<tr>
-              <td height="35" align="right">
-			  <div align="center" class="style5 style15"><strong>Address</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="address" type="text" size="50" placeholder="Enter Address......" />
-                </div></td>
-            </tr>
-			<tr>
-              <td height="31" align="right">
-			  <div align="center" class="style5 style15"><strong>E-mail</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="e_mail" type="text" size="50" placeholder="Enter e_mail......" />
-                </div></td>
-            </tr>
-			<tr>
-              <td height="35" align="right">
-			  <div align="center" class="style5 style15"><strong>Mobile Number </strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="a_r_claimed" type="text" size="50" placeholder="Enter Mobile Number......" />
-                </div></td>
-            </tr>
-			<tr>
-              <td height="32" align="right">
-			  <div align="center" class="style5 style15"><strong>Educational Qualification</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="edu_quali" type="text" size="50" placeholder="Enter Education qualification....." />
-                </div></td>
-            </tr>
-			<tr>
-              <td height="32" align="right">
-			  <div align="center" class="style5 style15"><strong>Upload Image</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="image" type="file" size="50"/>
-                </div></td>
-            </tr>
-			<tr>
-              <td height="30" align="right">
-			  <div align="center" class="style5 style15"><strong>Admission No</strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="reg_id" type="text" size="50" placeholder="Enter regstration-id......" />
-                </div></td>
-            </tr>
-			
-			<tr>
-              <td height="32" align="right">
-			  <div align="center" class="style5 style15"><strong>Roll Number </strong></div></td>
-              <td>
-			    
-		        <div align="left">
-		          <input name="agency" type="text" size="50" placeholder="Enter Roll Number......" />
-                </div></td>
-            </tr>
-		
-			
-			<tr>
-              <td align="center" colspan="6"><input type="submit" name="submit"  value="Published Now"></td>
-            </tr>
-          </table></td>
-        </tr>
-      </table></td>
-    </tr>
-  </table>
-  
-</form>
-
-
-
+<body>
+    <form action="" method="POST">
+      <label>User Name:</label>
+        <input type="text" name="username" placeholder="User Name:">
+        <br>
+        <label>Admission No:</label>
+        <input type="text" name="admno" placeholder="Admission No:">
+        <br>
+        <label>Phone No:</label>
+        <input type="text" name="phoneno" placeholder="Phone No:">
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" placeholder="Password"><br>
+        <label>Confirm Password:</label>
+        <input type="password" name="confirmpassword" placeholder="confirm Password"><br>
+        <button type="submit" name="register">Register</button>
+        <br>
+        Already Registered?<a href="index.php">login</a>
+    </form>
 </body>
 </html>
