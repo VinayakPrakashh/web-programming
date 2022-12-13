d78f7187e54ca6d81ddde5ee99433582d730dc14
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-13 20:45:02 +0530

    f

--- Cycle3/Q3(StudentLogin)/login.php
+++ Cycle3/Q3(StudentLogin)/login.php
@@ -20,16 +20,16 @@ else if(empty($pass)){
     header("Location: index.php?error=Password is required");
     exit();
 }
-$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+$sql = "SELECT * FROM users WHERE user_name='$uname' AND passwrd='$pass'";
 $result= mysqli_query($conn, $sql);
 if(mysqli_num_rows($result) === 1);
 $row = mysqli_fetch_assoc($result);
-if($row['user_name']=== $uname && $row['password'] === $pass){
+if($row['user_name']=== $uname && $row['passwrd'] === $pass){
     echo "logged In !";
     $_SESSION['user_name'] = $row['user_name'];
-    $_SESSION['m\name'] = $row['name'];
+    $_SESSION['mname'] = $row['name'];
     $_SESSION['id'] = $row['id'];
-    header("location: home2.php");
+    header("location: home.php");
     exit();
 }
 else{
---
+++ Cycle3/Q3(StudentLogin)/register.php
@@ -0,0 +1,34 @@
+<?php
+require_once('db_connection.php');
+?>
+<html>
+
+    
+    <head>REGISTRATION
+</head>
+<?php
+if (isset($_POST['register'])) {
+    $uname = $_POST['username'];
+    $admissionno = $_POST['admno'];
+    $phone = $_POST['phoneno'];
+    $passwrd = $_POST['password'];
+   mysqli_query($conn,"INSERT INTO users (user_name,admno,phone,passwrd) VALUES('$uname','$admissionno','$phone','$passwrd')");
+}
+?>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="username" placeholder="User Name:">
+        <br>
+        <input type="text" name="admno" placeholder="Admission No:">
+        <br><input type="text" name="phoneno" placeholder="Phone No:">
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password"><br>
+        <label>Confirm Password</label>
+        <input type="password" name="confirmpassword" placeholder="confirm Password"><br>
+        <button type="submit" name="register">register</button>
+        <br>
+        Already Registered?<a href="index.php">login</a>
+    </form>
+</body>
+</html>
