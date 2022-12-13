3b94ec575741a2da66721fd2f42cc3e2b94efa94
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-13 20:55:02 +0530

    g

--- Cycle3/Q3(StudentLogin)/register.php
+++ Cycle3/Q3(StudentLogin)/register.php
@@ -17,16 +17,21 @@ if (isset($_POST['register'])) {
 ?>
 <body>
     <form action="" method="POST">
+
+       <label>User Name:</label>
         <input type="text" name="username" placeholder="User Name:">
         <br>
+        <label>Admission No:</label>
         <input type="text" name="admno" placeholder="Admission No:">
-        <br><input type="text" name="phoneno" placeholder="Phone No:">
         <br>
-        <label>Password</label>
+        <label>Phone No:</label>
+        <input type="text" name="phoneno" placeholder="Phone No:">
+        <br>
+        <label>Password:</label>
         <input type="password" name="password" placeholder="Password"><br>
-        <label>Confirm Password</label>
+        <label>Confirm Password:</label>
         <input type="password" name="confirmpassword" placeholder="confirm Password"><br>
-        <button type="submit" name="register">register</button>
+        <button type="submit" name="register">Register</button>
         <br>
         Already Registered?<a href="index.php">login</a>
     </form>
