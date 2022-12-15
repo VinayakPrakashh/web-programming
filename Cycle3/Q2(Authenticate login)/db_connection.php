2ea94d2a674e89cf8eeecbe339f9ecccf7c5e387
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-15 22:02:14 +0530

    hg

--- Cycle3/Q2(Authenticate login)/db_connection.php
+++ Cycle3/Q2(Authenticate login)/db_connection.php
@@ -1,9 +1,9 @@
 <?php
-$sname = "localhost";
-$uname = "root";
-$password = "";
-$db_name = "test_db";
-$conn = mysqli_connect($sname, $uname, $password, $db_name);
-if (!$conn) {
-    echo "Connection failed";
-}
+ $sname = "localhost";
+ $uname = "root";
+ $password = "";
+ $db_name = "test_db";
+ $conn = mysqli_connect($sname, $uname, $password, $db_name);
+ if (!$conn) {
+     echo "Connection failed";
+ }
--- Cycle3/Q2(Authenticate login)/login.php
+++ Cycle3/Q2(Authenticate login)/login.php
@@ -17,17 +17,17 @@ if(empty($uname)){
     exit();
 }
 else if(empty($pass)){
-    header("Location: index.php?eroor=Password is required");
+    header("Location: index.php?error=Password is required");
     exit();
 }
-$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+$sql = "SELECT * FROM users WHERE user_name='$uname' AND passwrd='$pass'";
 $result= mysqli_query($conn, $sql);
 if(mysqli_num_rows($result) === 1);
 $row = mysqli_fetch_assoc($result);
-if($row['user_name']=== $uname && $row['password'] === $pass){
+if($row['user_name']=== $uname && $row['passwrd'] === $pass){
     echo "logged In !";
     $_SESSION['user_name'] = $row['user_name'];
-    $_SESSION['m\name'] = $row['name'];
+    $_SESSION['mname'] = $row['name'];
     $_SESSION['id'] = $row['id'];
     header("location: home.php");
     exit();
