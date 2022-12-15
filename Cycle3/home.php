d68c656130042ebb5122719243600574fdc31a21
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-16 03:34:49 +0530

    dsdfse

--- Cycle3/home.php
+++ Cycle3/home.php
@@ -3,7 +3,7 @@
         <h1>Name: <?php echo $_POST['fname']; ?> <?php echo $_POST['fname']; ?></h1>
         <br>
         Date of Birth: <h1><?php echo $_POST['date']; ?></h1><br>
-        Gender: <h1><?php echo $_POST['gender']; ?></h1><br>
+        Gender: <h1><?php echo $_POST["gender"]; ?></h1><br>
         Branch: <h1><?php echo $_POST['branch']; ?></h1><br>
         Semester: <h1><?php echo $_POST['branch2']; ?></h1><br>
         Phone No: <h1><?php echo $_POST['number']; ?></h1><br>
