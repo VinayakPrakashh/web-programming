b1e209d63f22848e62cdaf611e6a007fffc4c958
Vinayak Prakash <vinayakprakash2121@gmail.com>
2022-12-19 17:33:23 +0530

    rte

---
+++ Cycle3/Q2(Authenticate Login).php
@@ -0,0 +1,40 @@
+<html>
+
+    <head>
+        <title>LOGIN</title>
+        
+      
+    </head>
+    <?php
+   session_start();
+   if(array_key_exists('submit',$_POST))
+    {
+        $username2 = "vinayak";
+        $password2 = 12345;
+        if (($_POST['uname'] == $username2) && ($_POST['password'] == $password2)) {
+            $_SESSION['uname'] = $username2;
+              $_SESSION['password'] = $password2;
+         echo '<script>alert("Login Success")</script>';
+    
+        
+        }
+else{
+        echo '<script>alert("Login Failed")</script>';
+}
+    }
+    ?>
+    <body>
+        <form method="post"><h2>LOGIN</h2>
+        <?php if(isset($_GET['error'])){ ?>
+            
+        <p class="error"> <?php echo $_GET['error']; ?></p>
+        <?php } ?>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name"><br>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password"><br>
+
+        <button type="submit" name="submit" value="submit" onclick="validate()">LOGIN</button>
+    </form>
+    </body>
+</html>
--- Cycle3/Q2/home.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <h1>Hi,
-            <?php  session_start();
-            echo $_SESSION['uname']; ?></h1>
-        <br>
-       <h1> Your password=<?php echo $_SESSION['password']; ?></h1><br>
-    </body>
-</html>
